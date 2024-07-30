1ebdce40c9ac4a80ef778065f11553c96124d911
Andy Almeida <73726361+andyalpa@users.noreply.github.com>
2024-07-30 19:30:56 +0000

    Argument generator project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let letter = "A ";
+  let adjetive = ["tall ", "skinny ", "fat ", "pretty "];
+  let noun = ["cow, ", "dog ", "cat ", "pig "];
+  let verb = ["kick my ", "bite my ", "throw my ", "grab my "];
+  let object = ["lunch ", "phone ", "camera ", "homework "];
+  let location = [
+    "in my yard",
+    "inside the garage",
+    "on the roof",
+    "in the library"
+  ];
+
+  let random1 = Math.floor(Math.random() * adjetive.length);
+  let random2 = Math.floor(Math.random() * noun.length);
+  let random3 = Math.floor(Math.random() * verb.length);
+  let random4 = Math.floor(Math.random() * object.length);
+  let random5 = Math.floor(Math.random() * location.length);
+
+  document.querySelector("#argument").innerHTML =
+    letter +
+    adjetive[random1] +
+    noun[random2] +
+    verb[random3] +
+    object[random4] +
+    location[random5];
 };
